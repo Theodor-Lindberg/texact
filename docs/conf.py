7825599d8a769bcfcf5864ef0de00eacbb58c669
Theodor Lindberg <theodor.lindberg@liu.se>
2026-08-14 13:14:40 +0200

    Add changelog, and linkt to docs

--- docs/conf.py
+++ docs/conf.py
@@ -31,3 +31,15 @@ exclude_patterns = []
 html_theme = "furo"
 html_static_path = ["_static"]
 html_favicon = "_static/texact.ico"
+
+
+def setup(app):
+    import m2r2
+
+    m2r2._IS_SPHINX = True
+    app.add_config_value("no_underscore_emphasis", False, "env")
+    app.add_config_value("m2r_parse_relative_links", False, "env")
+    app.add_config_value("m2r_anonymous_references", False, "env")
+    app.add_config_value("m2r_disable_inline_math", False, "env")
+    app.add_config_value("m2r_use_mermaid", False, "env")
+    app.add_directive("mdinclude", m2r2.MdInclude)
