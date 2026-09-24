fcbeaaeae69e883deb8734d7b3c737d7ee318fb1
Theodor Lindberg <theodor.lindberg.se@gmail.com>
2026-09-24 20:38:23 +0200

    Try fixing docs build

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ author = "Theodor Lindberg"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "sphinx_multiversion"]
+extensions = ["myst_parser", "sphinx_multiversion", "m2r2.mdinclude"]
 source_suffix = {
     ".rst": "restructuredtext",
     ".md": "markdown",
@@ -68,15 +68,3 @@ html_theme_options = {
         "color-brand-visited": "#d1a221",
     },
 }
-
-
-def setup(app):
-    import m2r2
-
-    m2r2._IS_SPHINX = True
-    app.add_config_value("no_underscore_emphasis", False, "env")
-    app.add_config_value("m2r_parse_relative_links", False, "env")
-    app.add_config_value("m2r_anonymous_references", False, "env")
-    app.add_config_value("m2r_disable_inline_math", False, "env")
-    app.add_config_value("m2r_use_mermaid", False, "env")
-    app.add_directive("mdinclude", m2r2.MdInclude)
